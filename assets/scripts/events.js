e073d0341a3979e8f7e0ffbc1e5a553b94056a30
Vrinda Menon <vrindanm@gmail.com>
2021-03-21 15:31:47 +0530

    Fullscreen Menu

---
+++ assets/scripts/events.js
@@ -0,0 +1,13 @@
+
+let primaryNav = document.getElementById("primary-nav");
+
+function closeMenu(){
+
+    primaryNav.style.top = "-100vw";
+
+}
+
+function openMenu(){
+
+    primaryNav.style.top = "0";
+}
